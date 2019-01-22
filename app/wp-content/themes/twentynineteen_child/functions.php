3ab083c6a2714571088d6f051c97c597271986e6
wanghao8080 <117663444@qq.com>
2019-01-22 16:10:23 +0800

    自定义主题样式（创建子主题）

---
+++ app/wp-content/themes/twentynineteen_child/functions.php
@@ -0,0 +1,6 @@
+<?php
+
+function twentynineteen_child_enqueue_styles() {
+  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'twentynineteen_child_enqueue_styles' );
