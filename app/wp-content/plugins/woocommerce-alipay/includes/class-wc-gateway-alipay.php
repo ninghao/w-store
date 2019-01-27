afabcc0f02aa0f7012ca6a0ac1fb4f700fad4044
wanghao8080 <117663444@qq.com>
2019-01-27 12:09:38 +0800

    设置支付方法相关信息（id，标题，描述 ... ）

--- app/wp-content/plugins/woocommerce-alipay/includes/class-wc-gateway-alipay.php
+++ app/wp-content/plugins/woocommerce-alipay/includes/class-wc-gateway-alipay.php
@@ -5,5 +5,14 @@ if( ! defined( 'ABSPATH' ) ) {
 }
 
 class WC_Gateway_Alipay extends WC_Payment_Gateway {
-
+  public function __construct() {
+    $this->id                 = 'alipay';
+    $this->has_fields         = false;
+    $this->order_button_text  = '使用支付宝支付';
+    $this->method_title       = '支付宝';
+    $this->method_description = '使用支付宝支付';
+    $this->supports           = array(
+      'products'
+    );
+  }
 }
