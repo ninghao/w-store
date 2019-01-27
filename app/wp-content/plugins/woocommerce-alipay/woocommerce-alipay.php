ca278850f88b854bcf790da5edef2538363a8c46
wanghao8080 <117663444@qq.com>
2019-01-27 09:35:46 +0800

    创建支付宝插件

---
+++ app/wp-content/plugins/woocommerce-alipay/woocommerce-alipay.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  WooCommerce Alipay Payment Gateway
+Plugin URI:   https://ninghao.net
+Description:  Alipay payment gateway for WooCommerce.
+Version:      1.0.0
+Author:       ninghao.net
+Author URI:   https://ninghao.net
+*/
+
+if( ! defined( 'ABSPATH' ) ) {
+  exit;
+}
