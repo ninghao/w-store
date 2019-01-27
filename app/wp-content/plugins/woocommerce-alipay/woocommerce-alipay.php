59882ea400e37348243762cf9a9a9448e48002e2
wanghao8080 <117663444@qq.com>
2019-01-27 11:08:31 +0800

    创建 WooCommerce 支付方法

--- app/wp-content/plugins/woocommerce-alipay/woocommerce-alipay.php
+++ app/wp-content/plugins/woocommerce-alipay/woocommerce-alipay.php
@@ -11,3 +11,18 @@ Author URI:   https://ninghao.net
 if( ! defined( 'ABSPATH' ) ) {
   exit;
 }
+
+add_action( 'plugins_loaded', 'woocommerce_alipay_init' );
+
+function woocommerce_alipay_init() {
+  class WC_Gateway_Alipay extends WC_Payment_Gateway {
+
+  }
+}
+
+add_filter( 'woocommerce_payment_gateways', 'woocommerce_alipay_gateway_class' );
+
+function woocommerce_alipay_gateway_class( $methods ) {
+  $methods[] = 'WC_Gateway_Alipay';
+  return $methods;
+}
