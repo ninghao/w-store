388733428a18f27fa8fb95460eb6f3a73309a76c
wanghao8080 <117663444@qq.com>
2019-01-29 21:43:50 +0800

    生成支付宝支付链接：数据（biz_content）

--- app/wp-content/plugins/woocommerce-alipay/includes/class-wc-gateway-alipay-request.php
+++ app/wp-content/plugins/woocommerce-alipay/includes/class-wc-gateway-alipay-request.php
@@ -10,6 +10,22 @@ class WC_Gateway_Alipay_Request {
   }
 
   public function get_request_url( $order ) {
+    $out_trade_no = $order->get_id();
+    $subject = get_bloginfo( 'name' ) . ': # ' . $out_trade_no;
+    $total_amount = $order->get_total();
+    $product_code = 'FAST_INSTANT_TRADE_PAY';
+
+    $biz_content_raw = array(
+      'out_trade_no' => $out_trade_no,
+      'subject' => $subject,
+      'total_amount' => $total_amount,
+      'product_code' => $product_code,
+    );
+
+    $biz_content = json_encode( $biz_content_raw, JSON_UNESCAPED_UNICODE );
+
+    WC_Gateway_Alipay::log( $biz_content, 'debug', true );
+
     $request_url = 'https://ninghao.net';
     return $request_url;
   }
