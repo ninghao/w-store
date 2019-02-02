d36e2c72bea23bb79310f1feab4921ea9e37ab14
wanghao8080 <117663444@qq.com>
2019-02-02 20:11:37 +0800

    创建微信支付插件

---
+++ app/wp-content/plugins/woocommerce-wxpay/woocommerce-wxpay.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name:      WooCommerce Wxpay Payment Gateway
+Plugin URI:       https://ninghao.net
+Description:      Wxpay payment gateway for WooCommerce.
+Version:          1.0.0
+Author:           ninghao.net
+Author URI:       https://ninghao.net
+*/
+
+if( ! defined( 'ABSPATH' ) ) {
+  exit;
+}
+
+define( 'WC_WXPAY', plugin_dir_path( __FILE__ ) );
