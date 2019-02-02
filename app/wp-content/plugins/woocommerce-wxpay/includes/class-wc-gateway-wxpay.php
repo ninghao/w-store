ef51c86410c24bdb139c057107df62f2479e87c6
wanghao8080 <117663444@qq.com>
2019-02-02 20:54:09 +0800

    添加调试日志功能

--- app/wp-content/plugins/woocommerce-wxpay/includes/class-wc-gateway-wxpay.php
+++ app/wp-content/plugins/woocommerce-wxpay/includes/class-wc-gateway-wxpay.php
@@ -5,6 +5,9 @@ if( ! defined( 'ABSPATH' ) ) {
 }
 
 class WC_Gateway_Wxpay extends WC_payment_Gateway {
+  public static $log_enabled;
+  public static $log;
+
   public function __construct() {
     $this->id                 = 'wxpay';
     $this->has_fields         = false;
@@ -24,9 +27,25 @@ class WC_Gateway_Wxpay extends WC_payment_Gateway {
     $this->description        = $this->get_option( 'description' );
     $this->debug              = $this->get_option( 'debug', 'no' ) === 'yes';
     $this->sandbox            = $this->get_option( 'sandbox', 'no' ) === 'yes';
+
+    self::$log_enabled        = $this->debug;
   }
 
   public function init_form_fields() {
     $this->form_fields = include WC_WXPAY . 'includes/settings-wxpay.php';
+  }
+
+  public static function log( $message, $level = 'info', $return = false ) {
+    if ( self::$log_enabled ) {
+      if ( empty( self::$log ) ) {
+        self::$log = wc_get_logger();
+      }
+
+      if ( $return ) {
+        $message = wc_print_r( $message, true );
+      }
+
+      self::$log->log( $level, $message, array( 'source' => 'wxpay' ) );
+    }
   }
 }
