d3a8b36fcb77289a17dcfbd4de7ae45303357529
wanghao8080 <117663444@qq.com>
2019-02-10 14:45:42 +0800

    身份验证接口（JWT ）

---
+++ app/wp-content/plugins/w-store/includes/jwt-config.php
@@ -0,0 +1,8 @@
+<?php
+
+if( ! defined( 'ABSPATH' ) ) {
+  exit;
+}
+
+define( 'JWT_AUTH_SECRET_KEY', '2M+L06XXii1/tDMtrUuo8Irnq8bKshhg33wyA8RiSvV/4fYoH6hpTnU826mctcF5' );
+define( 'JWT_AUTH_CORS_ENABLE', true );
---
+++ app/wp-content/plugins/w-store/w-store.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name:  W-Store
+Plugin URI:   https://ninghao.net
+Description:  W-Store.
+Version:      1.0.0
+Author:       ninghao.net
+Author URI:   https://ninghao.net
+*/
+
+if( ! defined( 'ABSPATH' ) ) {
+  exit;
+}
+
+define( 'WS', plugin_dir_path( __FILE__ ) );
+
+require_once WS . 'includes/jwt-config.php';
