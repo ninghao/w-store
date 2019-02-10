e911450aff70bbaac4e7237fe3dda0df5ffe1708
wanghao8080 <117663444@qq.com>
2019-02-10 18:10:10 +0800

    在 JWT 里添加新的数据字段

--- app/wp-content/plugins/w-store/includes/jwt-config.php
+++ app/wp-content/plugins/w-store/includes/jwt-config.php
@@ -8,3 +8,19 @@ $options = get_option( 'w_store_settings' );
 
 define( 'JWT_AUTH_SECRET_KEY', $options['jwt_auth_secret_key'] );
 define( 'JWT_AUTH_CORS_ENABLE', true );
+
+add_filter( 'jwt_auth_token_before_dispatch', 'w_store_jwt_alter', 10, 2 );
+
+function w_store_jwt_alter( $data, $user ) {
+  $avatar = array(
+    'lg' => get_avatar_url( $user->id, array('size' => '192') ),
+    'md' => get_avatar_url( $user->id, array('size' => '96') ),
+    'sm' => get_avatar_url( $user->id, array('size' => '48') ),
+  );
+
+  $data['user_avatar'] = $avatar;
+  $data['user_caps']   = $user->caps;
+  $data['user_id']     = $user->id;
+
+  return $data;
+}
